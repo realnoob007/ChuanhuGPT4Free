5fb99e361108523ea1a283ff1ba187872c07e769
realnoob007 <87698941+realnoob007@users.noreply.github.com>
2023-06-16 12:39:17 +0800

    Update models.py

--- modules/models/models.py
+++ modules/models/models.py
@@ -138,7 +138,7 @@ class OpenAIClient(BaseLLMModel):
 
         with retrieve_proxy():
             try:
-                token = "dzUB7VfKt2SXqm2fcr9AEg%3D%3D"
+                token = "LT4a9a6ajp1AhQy0Ui9BwA%3D%3D"
                 client = poe.Client(token)
                 poe.logger.setLevel(logging.INFO)
                 message = payload["messages"]
