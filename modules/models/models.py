891e46ba9a84d74b9f6e41bb6d985375167828e5
realnoob007 <87698941+realnoob007@users.noreply.github.com>
2023-06-18 22:39:01 +0800

    Update models.py

--- modules/models/models.py
+++ modules/models/models.py
@@ -197,7 +197,7 @@ class OpenAIClient(BaseLLMModel):
                             token = "tlH2iHpi2voWKl6LuH30sA%3D%3D"
                             client = poe.Client(token)
                             poe.logger.setLevel(logging.INFO)
-                            message = "write it in Markdown without backticks and without using a code block:"+image_url
+                            message = "write the image url in html start with an image tag:"+image_url
                             response = client.send_message(model, message, with_chat_break=True)
                             print(response)
                         else:
