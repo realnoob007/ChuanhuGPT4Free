836eaf6b9284a2ae58e800506793489927d116bb
realnoob007 <87698941+realnoob007@users.noreply.github.com>
2023-07-08 22:56:27 +0800

    update display method

--- modules/models/models.py
+++ modules/models/models.py
@@ -206,7 +206,7 @@ class OpenAIClient(BaseLLMModel):
                             token = poe_token
                             client = poe.Client(token)
                             poe.logger.setLevel(logging.INFO)
-                            message = 'write the image url in html format but not in code block: '+image_url
+                            message = "display the image url first, then render it into markdown:"+image_url
                             response = client.send_message(model, message, with_chat_break=True)
                             print(response)
                         else:
@@ -258,7 +258,7 @@ class OpenAIClient(BaseLLMModel):
                                 token = poe_token
                                 client = poe.Client(token)
                                 poe.logger.setLevel(logging.INFO)
-                                message = 'write the image url without using []() in the format like this <img src="https://cdn.discordapp.com/attachments/1126752714429321320/1127236437629210714/hernandeznicole_7109805271309167_spiderman_realistic_photograph_b56bc980-e3c3-480a-943b-a4823c22bc68.png">'+": "+upscale_url
+                                message = "display the image url first, then render it into markdown:"+upscale_url
                                 response = client.send_message(model, message, with_chat_break=True)
                                 print(response)
                             else:
@@ -310,7 +310,7 @@ class OpenAIClient(BaseLLMModel):
                                 token = poe_token
                                 client = poe.Client(token)
                                 poe.logger.setLevel(logging.INFO)
-                                message = 'write the image url without using []() in the format like this <img src="https://cdn.discordapp.com/attachments/1126752714429321320/1127236437629210714/hernandeznicole_7109805271309167_spiderman_realistic_photograph_b56bc980-e3c3-480a-943b-a4823c22bc68.png">'+": "+variation_url
+                                message = "display the image url first, then render it into markdown:"+variation_url
                                 response = client.send_message(model, message, with_chat_break=True)
                                 print(response)
                             else:
